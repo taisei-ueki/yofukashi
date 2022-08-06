49f39ee04137872a8f9c47671f2462280b32de89
taisei_ueki <dachengzhimu044@gmail.com>
2022-08-06 13:46:52 +0000

    devise機能作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "ae82109cdc7b494aa6aaef2eba2c6174.vfs.cloud9.us-east-1.amazonaws.com"
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: "public/homes#top"
+  get 'public/homes/about' => 'public/homes#about', as: 'about'
+  
+  # ユーザ用
+  # URL /users/sign_in ...
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  namespace :admin do
+    root to: 'homes#top'
+  end
+  
+  namespace :public do
+  end
+
 end
