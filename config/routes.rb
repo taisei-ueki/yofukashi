d4b1f43c7504748ef5282e7280b5b5f91dfefa63
taisei_ueki <dachengzhimu044@gmail.com>
2022-08-23 06:06:58 +0000

    root直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'homes/top'
-  end
-  get 'posts/search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root to: "public/homes#top"
