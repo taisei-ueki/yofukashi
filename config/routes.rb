915fab58c3b1afedbb00b89af2888869597381e6
taisei_ueki <dachengzhimu044@gmail.com>
2022-08-23 06:36:05 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  root to: "public/homes#top"
+  root "public/homes#top"
   get 'public/homes/about' => 'public/homes#about', as: 'about'
   
   # ユーザ用
